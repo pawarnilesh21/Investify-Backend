cc6316ef729a855ca1f3cd968ed1950a0f2899a5
Pawar nilesh <pawarnil898@gmail.com>
2026-07-02 18:56:09 +0530

    update cors after deploy Frontend

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ await connectDB()
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'https://your-app-name.netlify.app'],
  credentials: true
 }))
 
